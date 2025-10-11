0ff78e9f74f3f7010d512543223109f4e679d59a
Kanu <erickfabiandonado@gmail.com>
2025-10-10 22:10:00 -0500

    Added requirements
    Also, the libraries were set initially to apply theem later

---
+++ main.py
@@ -0,0 +1,5 @@
+import sys
+import psutil
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel
+from PyQt6.QtGui import QPainter, QColor, QFont
+from PyQt6.QtCore import Qt, QTimer
